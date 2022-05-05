a4ce6eb6b78efed33d80a56d0d9d607416219087
Raphaela Vieira <80799294+raphaelavieira@users.noreply.github.com>
2022-05-05 13:29:37 -0300

    Update Maze.java

--- src/Maze.java
+++ src/Maze.java
@@ -23,9 +23,9 @@ public class Maze {
   ArrayStack line = new ArrayStack(1,0);
   ArrayStack column = new ArrayStack(1,0);
   ArrayStack lastPosition = new ArrayStack(1,0);
-
+  
+  //While not finding the exit print the position
   public void walkMaze() {
-
     inicialPosition();
     while (!findExit()) {
       printPosition();
@@ -33,48 +33,53 @@ public class Maze {
       printMaze();
     }
 
+  //Walk up
   if ((maze[line.lastPosition() - 1][column.lastPosition()] == " ") || (maze[line.lastPosition() - 1][column.lastPosition()] == "Y")) {
     var printPosition = maze[line.lastPosition() - 1][column.lastPosition()];
     line.push(line.lastPosition() - 1);
     lastPosition.push(0);
   }
-
+    
+  //Go right
   else if ((maze[line.lastPosition()][column.lastPosition() + 1] == " ") || (maze[line.lastPosition()][column.lastPosition() + 1] == "Y")) {
   var printPosition = maze[line.lastPosition()][column.lastPosition() + 1];
   column.push(column.lastPosition() + 1);
   lastPosition.push(1);
   }
-
+    
+  //Walk down
   else if ((maze[line.lastPosition() + 1][column.lastPosition()] == " ") || (maze[line.lastPosition() + 1][column.lastPosition()] == "Y")) {
     var printPosition = maze[line.lastPosition() + 1][column.lastPosition()];
     line.push(line.lastPosition() + 1);
     lastPosition.push(2);
   }
-
+    
+  //Go left
   else if ((maze[line.lastPosition()][column.lastPosition() - 1] == " ") || (maze[line.lastPosition()][column.lastPosition() - 1] == "B")) {
     var printPosition = maze[line.lastPosition()][column.lastPosition() - 1];
     column.push(column.lastPosition() - 1);
     lastPosition.push(3);
   }
-
+    
+  //Unstack function
   else {
     switch (lastPosition.lastPosition()) {
-
+      //Back down
       case 0:
         line.pop();
         lastPosition.pop();
         break;
-
+      //Go back left
       case 1:
         lastPosition.pop();
         column.pop();
         break;
-
+      //Back up
       case 2:
         lastPosition.pop();
         line.pop();
         break;
-
+      //Go back right
       case 3:
         column.pop();
         lastPosition.pop();
@@ -83,12 +88,13 @@ public class Maze {
     }
   }
 }
-
+//If found the exit
 public boolean findExit() {
   if (Objects.equals(maze[line.lastPosition()][column.lastPosition()], "Y")) {
     System.out.println("Exit found");
     return true;
   }
+  //If didn't find the exit
   else if (line.isEmpty() || column.isEmpty()) {
     System.out.println("Exit not found");
     return true;
@@ -96,6 +102,7 @@ public boolean findExit() {
   return false;
 }
 
+//Inicial position = [1][1]
 public void inicialPosition() {
   line.push(1);
   column.push(1);
@@ -103,10 +110,12 @@ public void inicialPosition() {
   System.out.println("Starting the Maze");
 }
 
+//Print a trail through the maze
 public void printPosition() {
   maze[line.lastPosition()][column.lastPosition()] = "@";
 }
 
+//Print the maze
 public void printMaze() {
   for (String[] strings : maze) {
     System.out.println(Arrays.toString(strings));
